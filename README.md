fb3dc9604853ef20ad91e411224c9a8e48875c74
ferric <ferric-sol@ferric.net>
2021-12-07 13:07:25 -0500

    Add websocket code to write to pythd and README

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # solsearcher
+## ETL to update a pyth oracle with floor and avg price for a given NFT collection
+
+### Running
+1. Create NFT Collection Product Account in the modified version of the pyth oracle (https://github.com/ferric-sol/pyth-client) 
+1. Create two Price Accounts one with price_type 'floor_price' and one with price_type 'avg_price' for this new Product Account
+1. Update product account id in solsearcher.js
+1. Update the collection names in solsearcher.js
+1. Run with ```node solsearcher.js``` and it should send the floor and average price for this NFT collection to the pythd running on localhost
+
+
+### TODO
+1. Run through every collection on every exchange and automatically create a product account id in pythd
+1. Create a mapping from marketplace collection name -> pyth product account id (different marketplaces name collections differently) -> solana address
--- solsearcher.js
+++ solsearcher.js
@@ -1,16 +1,64 @@
 const axios = require('axios').default;
+const WebSocket = require('ws');
+
+const pythProductAccountId = '4aDoSXJ5o3AuvL7QFeR6h44jALQfTmUUCTVGDD6aoJTM' // This is BTC/USD as an example but should map to the NFT Collection ID on pyth
+
+const pythSolanaArtPublisherKey = '';
+const pythMagicEdenPublisherKey = '';
+const pythAlphaArtPublisherKey = '';
+const pythExchangeArtPublisherKey = '';
+const pythDigitalEyesPublisherKey = '';
+const pythFTXPublisherKey = '';
+
+const update_price = async (publisherKey, accountId, floorPrice, avgPrice) => {
+    const ws = new WebSocket('ws://localhost/'); // location of the pythd
+
+    const floorPriceObj = {
+        "jsonrpc": "2.0",
+        "method": "update_price",
+        "params" : {
+          "account": accountId,
+          "price" : floorPrice,
+          "price_type": "floor_price",
+          "conf" : 3,
+          "status": "trading"
+        },
+        "id" : 1
+      }
+    ws.send(JSON.stringify(floorPriceObj));
+
+    const avgPriceObj = {
+        "jsonrpc": "2.0",
+        "method": "update_price",
+        "params" : {
+          "account": accountId,
+          "price" : avgPrice,
+          "price_type": "avg_price",
+          "conf" : 3,
+          "status": "trading"
+        },
+        "id" : 1
+      }
+    ws.send(JSON.stringify(avgPriceObj));
+}
 
 const getSolanaArt = async (collectionName) => {
     try { 
         // const collectionsResponse = await axios.get('https://qzlsklfacc.medianetwork.cloud/get_collections')
         // const collectionName = collectionsResponse.data[0].url;
         const floorResponse = await axios.get('https://qzlsklfacc.medianetwork.cloud/query_volume_per_collection?collection='+collectionName);
-        console.log('solanaart floor price:', floorResponse.data.floorPrice);
+
+        const floorPrice = floorResponse.data.floorPrice;
+        console.log('solanaart floor price:', floorPrice);
+
         const priceResponse = await axios.get('https://qzlsklfacc.medianetwork.cloud/nft_for_sale?collection='+collectionName);
-        console.log('item price: ', priceResponse.data[0].price);
         const reducer = (previousValue, currentValue) => previousValue + currentValue;
         const sum = priceResponse.data.map(item => item.price).reduce(reducer);
-        console.log('solanaart average price of all items: ', sum/priceResponse.data.length);
+
+        const avgPrice = sum/priceResponse.data.length;
+        console.log('solanaart average price of all items: ', avgPrice);
+
+        update_price(pythSolanaArtPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
@@ -28,6 +76,8 @@ const getMagicEden = async (collectionName) => {
         const priceResponse = await axios.get('https://api-mainnet.magiceden.io/rpc/getCollectionEscrowStats/'+collectionName);
         const avgPrice = priceResponse.data.results.avgPrice24hr/Math.pow(10, exponent);
         console.log('magiceden avg price: ', avgPrice);
+
+        update_price(pythMagicEdenPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
@@ -40,8 +90,10 @@ const getAlphaArt = async (collectionName) => {
         // const collectionName = collectionsResponse.data.collections[0].slug;
         const floorResponse = await axios.get('https://apis.alpha.art/api/v1/collection/'+collectionName);
         const exponent = 9;
+
         const floorPrice = floorResponse.data.floorPrice/Math.pow(10, exponent);
         console.log('alpha art floor price:', floorPrice);
+
         const priceResponse = await axios.post('https://apis.alpha.art/api/v1/collection', {
             "collectionId":collectionName,
             "orderBy":"PRICE_LOW_TO_HIGH",
@@ -54,6 +106,8 @@ const getAlphaArt = async (collectionName) => {
 
         const avgPrice = sum/priceResponse.data.tokens.length;
         console.log('alpha art average price of all items: ', avgPrice/Math.pow(10, exponent));
+
+        update_price(pythAlphaArtPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
@@ -66,14 +120,18 @@ const getExchangeArt = async (collectionName) => {
         // const collectionName = collectionsResponse.data[0].collectionName;
         const floorResponse = await axios.get('https://api.exchange.art/v1/public/collections/metadata?collectionName='+collectionName);
         const exponent = 9;
+
         const floorPrice = floorResponse.data.floorPrice/Math.pow(10, exponent);
         console.log('exchange art floor price:', floorPrice);
+
         const priceResponse = await axios.get('https://api.exchange.art/v1/public/tokens?collections='+collectionName);
         const reducer = (previousValue, currentValue) => previousValue + currentValue;
         const sum = priceResponse.data.tokens.map(item => item.lastListedPrice).reduce(reducer);
 
         const avgPrice = sum/priceResponse.data.tokens.length;
         console.log('exchange art average price of all items: ', avgPrice/Math.pow(10, exponent));
+
+        update_price(pythExchangeArtPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
@@ -86,14 +144,18 @@ const getDigitalEyes = async (collectionName) => {
         // const collectionName = collectionsResponse.data[0].name;
         const floorResponse = await axios.get('https://us-central1-digitaleyes-prod.cloudfunctions.net/offers-retriever?collection='+collectionName);
         const exponent = 9;
+        j
         const floorPrice = floorResponse.data.price_floor/Math.pow(10, exponent);
         console.log('exchange art floor price:', floorPrice);
+
         const priceResponse = await axios.get('https://us-central1-digitaleyes-prod.cloudfunctions.net/offers-retriever?collection='+collectionName);
         const reducer = (previousValue, currentValue) => previousValue + currentValue;
         const sum = priceResponse.data.offers.map(item => item.price).reduce(reducer);
 
         const avgPrice = sum/priceResponse.data.offers.length;
         console.log('exchange art average price of all items: ', avgPrice/Math.pow(10, exponent));
+
+        update_price(pythExchangeArtPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
@@ -105,8 +167,10 @@ const getFTX = async (collectionName) => {
         // const collectionsResponse = await axios.get('https://ftx.com/api/nft/nfts'
         // const collectionName = collectionsResponse.data[0].name;
         const floorResponse = await axios.get('https://ftx.com/api/nft/collection/'+collectionName);
+
         const floorPrice = floorResponse.data.result.floor_price;
         console.log('ftx floor price:', floorPrice);
+
         const allItemsUrl = "https://ftx.com/api/nft/nfts_filtered?startInclusive=0&endExclusive=10000000&nft_filter_string=%7B%22collection%22%3A%22" + collectionName + "%22%2C%22nftAuctionFilter%22%3A%22all%22%2C%22minPriceFilter%22%3Anull%2C%22maxPriceFilter%22%3Anull%2C%22seriesFilter%22%3A%5B%5D%2C%22traitsFilter%22%3A%7B%7D%2C%22searchStringFilter%22%3Anull%2C%22mintSourceFilter%22%3Anull%2C%22include_not_for_sale%22%3Atrue%7D&sortFunc=offer_asc";
         const priceResponse = await axios.get(allItemsUrl);
         const reducer = (previousValue, currentValue) => previousValue + currentValue;
@@ -114,6 +178,8 @@ const getFTX = async (collectionName) => {
 
         const avgPrice = sum/priceResponse.data.result.nfts.length;
         console.log('exchange art average price of all items: ', avgPrice);
+
+        update_price(pythFTXPublisherKey, pythProductAccountId, floorPrice, avgPrice);
     } catch(err) {
         // Handle Error Here
         console.error(err);
